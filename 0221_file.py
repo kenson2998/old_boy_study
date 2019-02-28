79e4d4302b2f7a92a8916786a9e89a466e1e2a0e
傅凱昇 <leon@fukaishengde-MacBook-Air.local>
2019-03-01 06:59:38 +0800

    sys and file note

--- 0221_file.py
+++ 0221_file.py
@@ -1,7 +1,7 @@
 # 讀取
 f = open('file_p', 'r')
-data = f.read()
-print(data)
+# data = f.read()
+# print(data)
 f.close()
 
 # append 追加寫入
@@ -12,8 +12,8 @@ f.close()
 
 # 讀取前五行資料
 f = open('file_p', 'r')
-for i in range(4):
-    print(f.readline().strip())
+# for i in range(4):
+#     print(f.readline().strip())
 f.close()
 
 # 第四行不印
@@ -27,13 +27,22 @@ f.close()
 # f.close()
 
 # high bige
-f = open('file_p', 'r')
-count = 0
-for line in f:
-    if count == 3:
-        count += 1
-        print('不列印')
-        continue
-    print(line.strip())
-    count += 1
-f.close()
+# f = open('file_p', 'r')
+# count = 0
+# for line in f:
+#     if count == 3:
+#         count += 1
+#         print('不列印')
+#         continue
+#     print(line.strip())
+#     count += 1
+# f.close()
+
+# f = open('file_p','r')
+# print(f.readline())
+# print(f.readline())
+# print(f.tell()) #顯示光標位置
+# print(f.seek(0)) #讓光標回到0
+# print(f.readline())
+# f.close()
+
---
+++ 進度條.py
@@ -0,0 +1,7 @@
+import sys,time
+#standard 標準輸出 （螢幕上輸出）
+
+for i in range(20):
+    sys.stdout.write("# ")
+    sys.stdout.flush()
+    time.sleep(0.1)
