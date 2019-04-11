783a9ebbc91806f2c39ff779cf411bfb4ab129fd
Leon <kenson2998@gmail.com>
2019-04-11 09:45:11 +0800

    update checkcode

--- 0411_checkcode驗證碼.py
+++ 0411_checkcode驗證碼.py
@@ -1,6 +1,6 @@
 import random
 
-int_list = [[x for x in range(65,91)],[x for x in range(97,123)],[x for x in range(0,10)]]
+int_list = [[x for x in range(65, 91)], [x for x in range(97, 123)], [x for x in range(0, 10)]]
 checkcode = ''
 
 for i in range(4):
@@ -9,5 +9,18 @@ for i in range(4):
         checkcode += str(current[0])
     else:
         checkcode += str(chr(current[0]))
+
+print(checkcode)
+
+#  老師的寫法
+checkcode = ''
+
+for i in range(4):
+    current = random.randrange(0, 4)
+    if current == i:
+        tmp = chr(random.randint(65, 90))
+    else:
+        tmp = random.randint(0, 9)
+    checkcode += str(tmp)
 
 print(checkcode)
