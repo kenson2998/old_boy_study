e4ec7a9ab2674c502734077d288158a8a45a30f4
Leon <kenson2998@gmail.com>
2019-04-11 08:54:17 +0800

    update checkcode

--- 0411_checkcode驗證碼.py
+++ 0411_checkcode驗證碼.py
@@ -1,1 +1,13 @@
-import random 
+import random
+
+int_list = [[x for x in range(65,91)],[x for x in range(97,123)],[x for x in range(0,10)]]
+checkcode = ''
+
+for i in range(4):
+    current = random.sample(int_list[random.randint(0, 2)], 1)
+    if current[0] <= 10:
+        checkcode += str(current[0])
+    else:
+        checkcode += str(chr(current[0]))
+
+print(checkcode)
