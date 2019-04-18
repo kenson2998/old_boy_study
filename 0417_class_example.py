e62221b2e1e41ecbc87a8631f4700d5326503404
Leon <kenson2998@gmail.com>
2019-04-18 14:13:14 +0800

    update class note

--- 0417_class_example.py
+++ 0417_class_example.py
@@ -16,15 +16,20 @@ d3.bulk()
 
 
 class role:
+    n = 123  # 這是類變量
+    name = "我是類name"
+    n_list = []
+
     def __init__(self, name, role, weapon, life_value=100, money=10000):  # 生命和金錢已給個默認值,可寫可不寫
         # __init__ 叫做構造函數
-        self.name = name
+        # 在實例化時做一些類的初始化的工作
+        self.name = name  # 實例變量(靜態屬性),作用域就是實例本身
         self.role = role
         self.weapon = weapon
         self.life_value = life_value
         self.money = money
 
-    def shot(self):
+    def shot(self):  # 類的方法, 功能(動態屬性)
         print('%s use %s shooting...' % (self.name, self.weapon))
 
     def got_shot(self):
@@ -37,12 +42,26 @@ class role:
 
 
 r1 = role('Leon', 'police', 'b43')  # 創建一個角色
+r2 = role('Jack', 'terrorist', 'b41')  # 創建一個角色
 
 r1.shot()
 r1.got_shot()
 r1.got_shot()
 r1.buy_gun('B51')
 r1.shot()
+r1.bullet_prove = True  # 沒有的變數也可以在這邊生成
+# del r1.weapon # 也可以刪除
+
+r1.n = '456'  # 修改r1的n
+print(r1.n, r1.name, r1.bullet_prove, r1.weapon)
+print(r2.n, r2.name, r2.weapon)
+
+role.n = '修改n'  # 直接改class裡面的n
+print('r1.n: %s, r2.n: %s' % (r1.n, r2.n))
+
+r2.n_list.append('from r2')  # 但是list裡面如果修改, 對所有的都會一起修改
+
+print('role.n_list: %s, r1.n_list: %s, r2.n_list: %s' % (role.n_list, r1.n_list, r2.n_list))
 
 # 上述的 r1 = role('Leon', 'Police', 'b43')
 # 其實是 role(r1, 'Leon', 'Police', 'b43') ,把r1帶入到role()裡面了
