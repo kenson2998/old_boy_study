8c11e252a705488b9e65d94bf621e900a36a7a9c
Leon <kenson2998@gmail.com>
2019-06-07 18:10:20 +0800

    update to school inherit

---
+++ 0607_繼承_school.py
@@ -0,0 +1,76 @@
+class School(object):
+    def __init__(self, name, addr):
+        self.name = name
+        self.addr = addr
+        self.teachers = []
+        self.students = []
+
+    def enroll(self, stu_obj):
+        print("為學員%s辦理註冊手續" % stu_obj.name)
+        self.students.append(stu_obj)
+
+
+class SchoolMember(object):
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def tell(self):
+        pass
+
+
+class Teacher(SchoolMember):
+    def __init__(self, name, age, sex, salary, course):
+        super(Teacher, self).__init__(name, age, sex)
+        self.salary = salary
+        self.course = course
+
+    def tell(self):
+        print('''
+        ---- info of Teacher : %s
+        Name : %s
+        Age : %s
+        Sex : %s
+        Salary : %s
+        Course : %s
+        
+        ''' % (self.name,self.name, self.age, self.sex, self.salary, self.course))
+
+    def teach(self):
+        print("%s is teaching course [%s]" % (self.name, self.course))
+
+
+class Student(SchoolMember):
+    def __init__(self, name, age, sex, std_id, grade):
+        super(Student, self).__init__(name, age, sex)
+        self.std_id = std_id
+        self.grade = grade
+
+    def tell(self):
+        print('''
+        ---- info of Student : %s ----
+        Name : %s
+        Age : %s
+        Sex : %s
+        student_ID : %s
+        Grade : %s
+
+        ''' % (self.name,self.name, self.age, self.sex, self.std_id, self.grade))
+
+    def pay_tuition(self, amount):
+        print("%s has paid tuition $%s" % (self.name, amount))
+
+
+school = School("老男孩IT","沙河")
+
+t1 = Teacher("Oldboy",56,"MF",20000,"Linux")
+t2 = Teacher("Alex",22,"M",3000,"Python")
+
+s1 = Student("Leon",30,"M",1001,"python")
+s2 = Student("Leo",24,"M",1002,"python")
+
+t1.tell()
+t2.tell()
+s1.tell()
+s2.tell()
