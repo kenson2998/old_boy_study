1246cbeede142110e01f50fde5bd5781440a6c05
Leon <kenson2998@gmail.com>
2019-06-26 22:25:15 +0800

    update defin except

--- 0626_自定義異常_classExcept.py
+++ 0626_自定義異常_classExcept.py
@@ -1,11 +1,15 @@
-#自定義異常
+# 自定義異常
 class leonException(Exception):
-    def __init__(self,msg):
+    def __init__(self, msg):
         self.message = msg
-    def __str__(self):
+        self.sss = msg + "234"
+
+    def __str__(self):  # __str__ 用於定義返回的值為什麼格式
         return self.message
+        # return '我是異常例外喔'
+
 
 try:
-    raise leonException('我的異常')
+    raise leonException('我的異常')  # raise (瑞斯) 觸發的意思
 except leonException as e:
     print(e)
