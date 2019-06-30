2b64d3058555fb09a7cdd185a90e48b9c559520f
Leon <kenson2998@gmail.com>
2019-06-30 15:38:40 +0800

    update socket

--- socket_0626/0630_socket_client.py
+++ socket_0626/0630_socket_client.py
@@ -1,13 +1,21 @@
 import socket
 
-client = socket.socket()
+client = socket.socket()  # 聲明socket 類型,同時生成socket連接對象
 
-client.connect(('localhost', 6969))
+client.connect(('localhost', 6666))
 
-client.send(b"hello world") #python3 只能發byte類型的字節,python2 才可以發字符串
+while True:
+    msg = input("想發什麼:").strip()
+    # client.send(b"Hello world") #python3 只能發byte類型的字節,python2 才可以發字符串
+    # client.send("我要下載a片".encode("utf-8")) #python3 只能發byte類型的字節,python2 才可以發字符串
+    if len(msg) == 0: continue  # 如果輸入為空值,會直接卡死,所以用continue到下一次回圈
+    client.send(msg.encode("utf-8"))
 
-data = client.recv(1024)
+    data = client.recv(10240000)  # 接收的bytes 大小,如果設定太小有可能會無法接收過大的檔案
+    print("recv:", data.decode())
 
-print("recv:", data)
+    f = open('video 2.mov', 'wb')
+    f.write(data)
+    f.close()
 
 client.close()
--- socket_0626/0630_socket_server.py
+++ socket_0626/0630_socket_server.py
@@ -1,19 +1,31 @@
 import socket
-
+import os
 server = socket.socket()
 
-server.bind(('localhost', 6969))
+server.bind(('localhost', 6666))
 
-server.listen()  # 監聽
-
+server.listen(5)  # 最大允許監聽數
 print("我要開始等電話了")
-conn, addr = server.accept()  # 等待電話打進來, conn = 電話打過來的實例,addr = 客戶端口
-#conn 就是客戶端連過來而在服務器端為其生成的一個實例連結
-print(conn, addr)
-data = conn.recv(1024)  # 接收
 
-print("recv:", data)
+while True:  # 第一層 while結束會監聽下一位client
+    conn, addr = server.accept()  # 等待電話打進來, conn = 電話打過來的實例,addr = 客戶端口
+    # conn 就是客戶端連過來而在服務器端為其生成的一個實例連結
+    print(conn, addr)
+    while True: # 這個client 持續接收
+        data = conn.recv(1024)  # 接收
+        print("recv:", data.decode())
+        if not data:  # data 為空
+            print("client has lost...", data)
+            break
 
-conn.send(data.upper())  # 發送
+        # conn.send(data.upper())  # 發送大寫返回
+
+        # res = os.popen(data.decode()).read() #執行linux指令
+        # conn.send(res.encode("utf-8"))  # res指令結果
+
+
+        f = open("video 2.mov",'rb')
+        data = f.read()
+        conn.send(data)
 
 server.close()
